fa3831a0a0f0802eeec5f37db227f3bf10cc3e98
Tom Elliott <mastacontrola@users.noreply.github.com>
2016-02-20 09:48:49 -0500

    Fix searchButton.js
    The issue with the current CSE plugin is it tries to open a number of tabs when it is unnecessary.
    
    This should correct the problem by not trying to open multiple search items.

--- searchButton.js
+++ searchButton.js
@@ -1,40 +1,32 @@
+var html = '';
 $(function (){
-	var html = '<li class="">' ;
-	html += ' <form id="search-form" class="navbar-form navbar-right hidden-xs">';
-	html += '   <div id="search-fields">';
-	html += '    <div class="form-group">';
-	html += '       <span class="visible-xs-inline">Unread</span>';
-	html += '     <input id="txtQuery" type="text" class="form-control" placeholder="Search" name="search" value="">';
-	html += '   </div>';
-	html += '  <button id="search-submit" type="submit" class="btn btn-default"><i class="fa fa-search fa-fw" title="Search"></i></button>';
-	html += ' </div>';
-	html += ' </form>';
-	html += '</li>';
-
-	var navBar = $('#main-nav');
-	navBar.append(html);
-
-	var queryText = $('#txtQuery');
-	$('#search-submit').on('click',function() {
-		$('#search-form').submit(function(e) {
-			search(queryText.val());
-			queryText.val('');
-			e.preventDefault();
-		});
-	});
-	$('#search-form').on('submit', function(e) {
-		search(queryText.val());
-		queryText.val('');
-		e.preventDefault();
-	});
+    html += '<li class="">' ;
+    html += ' <form id="search-form" class="navbar-form navbar-right hidden-xs">';
+    html += '   <div id="search-fields">';
+    html += '    <div class="form-group">';
+    html += '       <span class="visible-xs-inline">Unread</span>';
+    html += '     <input id="txtQuery" type="text" class="form-control" placeholder="Search" name="search" value="">';
+    html += '   </div>';
+    html += '  <button id="search-submit" type="submit" class="btn btn-default"><i class="fa fa-search fa-fw" title="Search"></i></button>';
+    html += ' </div>';
+    html += ' </form>';
+    html += '</li>';
+    var navBar = $('#main-nav');
+    navBar.append(html);
+    var queryText = $('#txtQuery');
+    $('#search-form').submit(function(e) {
+        search(queryText.val());
+        queryText.val('');
+        e.preventDefault();
+    });
 });
 function search(data) {
-	if (typeof(data) == 'undefined') data = '';
-  var url = $.ajax({
-   type: 'GET',
-   url: '/CSE',
-   data: '',
-   async: false
-  }).responseText;
-  window.open(url+data, '_blank');
+    if (typeof(data) == 'undefined') return;
+    var url = $.ajax({
+        type: 'GET',
+        url: '/CSE',
+        data: '',
+        async: false
+    }).responseText;
+    window.open(url+data, '_blank');
 }
